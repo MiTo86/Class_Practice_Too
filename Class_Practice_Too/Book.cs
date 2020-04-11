b6a79f9bb768f97f2582b26af95fa8efa55700d7
M T <mishatosca8@gmail.com>
2020-04-11 14:03:36 -0400

    Add project files.

---
+++ Class_Practice_Too/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Practice_Too
+{
+    class Book
+    {
+        public string Title;
+        public string author;
+        public int pages;
+    }
+}
---
+++ Class_Practice_Too/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Class_Practice_Too
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
