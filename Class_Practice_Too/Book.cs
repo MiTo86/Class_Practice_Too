f2de4f9e41a65b21f76c22de1d666b5943bbec0b
M T <mishatosca8@gmail.com>
2020-04-12 01:09:31 -0400

    Added a constructor to the Book class.

--- Class_Practice_Too/Book.cs
+++ Class_Practice_Too/Book.cs
@@ -9,5 +9,19 @@ namespace Class_Practice_Too
         public string Title;
         public string Author;
         public int Pages;
+
+        //Adding a constructor - a special method that is put inside a class that is called whenever an object of the class is created.
+        public Book(string aTitle, string aAuthor, int aPages) // The constructor with parameters.
+        {
+            Title = aTitle;
+            Author = aAuthor;
+            Pages = aPages;
+        }
+
+        //Can also have a constructor without parameters
+        public Book()
+        {
+
+        }
     }
 }
--- Class_Practice_Too/Program.cs
+++ Class_Practice_Too/Program.cs
@@ -6,7 +6,7 @@ namespace Class_Practice_Too
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
+            /*Book book1 = new Book();
             book1.Title = "Forward the Foundation";
             book1.Author = "Isaac Asimov";
             book1.Pages = 434;
@@ -14,13 +14,21 @@ namespace Class_Practice_Too
             Book book2 = new Book();
             book2.Title = "The Last Emperox";
             book2.Author = "John Scalzi";
-            book2.Pages = 387;
+            book2.Pages = 387;*/
+
+            //With a constructor
+            Book book1 = new Book("Forward the Foundation", "Isaac Asimov", 434);
+            Book book2 = new Book("The Last Emperox", "John Scalzi", 387);
+            Book book3 = new Book();
+
+            book2.Title = "The Consuming Fire";
 
             Console.WriteLine(book1.Title);
             Console.WriteLine(book1.Author);
 
             Console.WriteLine(book2.Title);
             Console.WriteLine(book2.Author);
+
             Console.ReadLine();
         }
     }
